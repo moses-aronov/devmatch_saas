59576d286ef36f57859451cca2293dc5c6b0fa60
Moses Aronov <moses@aronov.me>
2017-01-13 20:16:21 +0000

    Created User show view

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,6 @@
 class UsersController < ApplicationController
     def show
         @user = User.find(params[:id]) #Look at rake routes, the new param is id
+        @profile = @user.profile
     end
 end
