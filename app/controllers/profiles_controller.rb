72d366107980d130b46d8d14f22fadb8cc8c0770
Moses Aronov <moses@aronov.me>
2017-01-13 17:51:29 +0000

    Create User Profile Controller

---
+++ app/controllers/profiles_controller.rb
@@ -0,0 +1,5 @@
+class ProfilesControllers < ApplicationController
+
+
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,13 @@
 Rails.application.routes.draw do
   devise_for :users, controllers: {registrations: 'users/registrations'}
-  resources :contacts
   
+  #Create a nested resource
+  
+  resource :users  do
+    resource :profile
+  end
+  
+  resources :contacts
   get '/about' => 'pages#about'
   
   root 'pages#home'
