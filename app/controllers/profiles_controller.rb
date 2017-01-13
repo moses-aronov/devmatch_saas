26a488bcfbf59b399d697b17bbf35d0d3445f76c
Moses Aronov <moses@aronov.me>
2017-01-13 20:24:40 +0000

    Change new action profile controller

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -3,7 +3,7 @@ class ProfilesController < ApplicationController
         # form where user can fill out their own profile
         @user = User.find(params[:user_id])
         
-        @profile = @user.build_profile
+        @profile = Profile.new
     end
 
 
