c03eaa316f8ed7c93467c014570af0c8b7984a1b
Moses Aronov <moses@aronov.me>
2017-01-13 19:59:19 +0000

    Added profiles form and create action

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -7,4 +7,27 @@ class ProfilesController < ApplicationController
     end
 
 
+    def create
+        #Identify user
+        @user = User.find(params[:user_id])
+        #build_profile creates a blank profile, pass the form into the build
+        @profile = @user.build_profile(profile_params)
+        
+        if @profile.save
+            flash[:success] = "Profile Updated!"
+            redirect_to user_path(params[:user_id])
+        else
+            render action: :new
+        end
+        
+    end
+    
+    
+    private
+        def profile_params
+            #whitelist the formfields that you are using
+            params.require(:profile).permit(:first_name, last_name, :job_title, :phone_number, :contact_email, :description)
+        end
+        
+    
 end
