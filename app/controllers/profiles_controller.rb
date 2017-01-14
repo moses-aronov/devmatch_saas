8390b66cd72765b4c4924e4a70d3ab99986f6faa
Moses Aronov <moses@aronov.me>
2017-01-14 13:04:09 +0000

    Added file fields for image upload on profiles

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -45,7 +45,7 @@ class ProfilesController < ApplicationController
     private
         def profile_params
             #whitelist the formfields that you are using
-            params.require(:profile).permit(:first_name, :last_name, :job_title, :phone_number, :contact_email, :description)
+            params.require(:profile).permit(:first_name, :last_name, :avatar, :job_title, :phone_number, :contact_email, :description)
         end
         
         def only_current_user
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,5 @@
 class UsersController < ApplicationController
     before_action :authenticate_user!
-    before_action :only_current_user
     
     def show
         @user = User.find(params[:id]) #Look at rake routes, the new param is id
